1c67a5bd428f009994b45f9af8b62ce189c8101f
Ryan McNair <ryan.mcnair@gmail.com>
2020-11-09 19:19:46 -0600

    firebase authentication added

---
+++ src/components/Auth/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import googleImage from './Sign-in-with-Google.png';
+
+export default class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  render() {
+    return (
+      <div className='Auth'>
+        <button className='btn btn-secondary' onClick={this.loginClickEvent}>
+          <img src={googleImage} alt='Google Sign in Button' />
+        </button>
+      </div>
+    );
+  }
+}
